ef97374bc808df662d769524d9a291b29e120e5c
CheaBunlong <c.bunlong168@gmail.com>
2025-09-06 20:54:24 +0700

    change deploy on chathub

--- obj/Debug/net9.0/EcommerceAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/EcommerceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d0a03059828c08c745e707540c2d2515211060d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95474c99ae69fc604deab7531e36e33174567be7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
