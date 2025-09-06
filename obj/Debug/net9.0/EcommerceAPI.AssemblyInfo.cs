05067499d1936323c90cb93028b928b8634e3ceb
CheaBunlong <c.bunlong168@gmail.com>
2025-09-06 18:36:37 +0700

    change deploy on sqlserver

--- obj/Debug/net9.0/EcommerceAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/EcommerceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79a5caf1409d2d327d3b22039ade1aff894ac735")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d0a03059828c08c745e707540c2d2515211060d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
